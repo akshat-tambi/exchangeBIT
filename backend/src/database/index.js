abf803696066b6e2314dc54faf1fc5f2259e4263
Akshat Tambi <142687802+akshat-tambi@users.noreply.github.com>
2024-06-11 20:01:39 +0530

    moved constants.js->env

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDb=async()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`DB connected successfully,`)
     } catch (error) {
         console.error("there is error in connecting the database",error);
