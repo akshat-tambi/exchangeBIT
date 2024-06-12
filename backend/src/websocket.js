edd2a7b75cf24f3e816720b42833e08f9572ded1
Akshat Tambi <142687802+akshat-tambi@users.noreply.github.com>
2024-06-12 09:21:06 +0530

    added checks in req system

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -47,149 +47,162 @@ wss.on('connection', (ws) => {
                 break;
                 
                 case 'ACCEPT_REQUEST':
-    {
-        // accept
-        const acceptedRequest = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'accepted' }, { new: true });
-        if (!acceptedRequest) {
-            console.error('Request not found');
-            return;
-        }
-
-        // clear product's requests array
-        await Product.findByIdAndUpdate(acceptedRequest.product, { requests: [] });
-
-        // mark product-status
-        await Product.findByIdAndUpdate(acceptedRequest.product, { status: true });
-
-        // add product to cart
-        const user = await User.findById(acceptedRequest.buyer);
-        user.cart.push(acceptedRequest.product);
-        await user.save();
-
-        // remove the request 
-        await User.findByIdAndUpdate(acceptedRequest.buyer, { $pull: { outgoingRequests: acceptedRequest._id } });
-
-        // remove the request 
-        await User.findByIdAndUpdate(acceptedRequest.owner, { $pull: { incomingRequests: acceptedRequest._id } });
-
-        //notify
-        const notificationMessage = `Your request for the product has been accepted`;
-        const notification = new Notification({ user: acceptedRequest.buyer, message: notificationMessage });
-        await notification.save();
-        await User.findByIdAndUpdate(acceptedRequest.buyer, { $push: { notif: notification._id } });
-        break;
-    }
-
-case 'REJECT_REQUEST':
-    {
-        const rejectedRequest = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'rejected' }, { new: true });
-        if (!rejectedRequest) {
-            console.error('Request not found');
-            return;
-        }
-
-        // remove
-        await User.findByIdAndUpdate(rejectedRequest.buyer, { $pull: { outgoingRequests: rejectedRequest._id } });
-
-        // remove
-        await Product.findByIdAndUpdate(rejectedRequest.product, { $pull: { requests: rejectedRequest._id } });
-
-        // remove
-        await User.findByIdAndUpdate(rejectedRequest.owner, { $pull: { incomingRequests: rejectedRequest._id } });
-
-        // notify
-        const rejectionMessage = `Your request for the product has been rejected`;
-        const rejectionNotification = new Notification({ user: rejectedRequest.buyer, message: rejectionMessage });
-        await rejectionNotification.save();
-        await User.findByIdAndUpdate(rejectedRequest.buyer, { $push: { notif: rejectionNotification._id } });
-        break;
-    }
-
-    case 'REQUEST_REQUEST':
-        const product = await Product.findById(parsedMessage.productId);
-        const request = new Request({
-            owner: product.user,
-            buyer: parsedMessage.from,
-            product: parsedMessage.productId
-        });
-        await request.save();
-    
-        // add
-        await User.findByIdAndUpdate(parsedMessage.from, { $push: { outgoingRequests: request._id } });
-    
-        //add
-        await Product.findByIdAndUpdate(parsedMessage.productId, { $push: { requests: request._id } });
-    
-        // add
-        await User.findByIdAndUpdate(product.user, { $push: { incomingRequests: request._id } });
-    
-        // notify
-        const requestNotificationMessage = `You have received a request for your product`;
-        const requestNotification = new Notification({ user: product.user, message: requestNotificationMessage });
-        await requestNotification.save();
-        await User.findByIdAndUpdate(product.user, { $push: { notif: requestNotification._id } });
-        break;
-    
-
-    case 'CANCEL_DEAL_OWNER':
-    {   
-        const request = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'cancelled' }, { new: true });
-        if (!request) {
-            console.error('Request not found');
-            return;
-        }
-
-        // updating the product status
-        const product = await Product.findByIdAndUpdate(request.product, { status: false });
-        if (!product) {
-            console.error('Product not found');
-            return;
-        }
-
-        // remove the product
-        const user = await User.findByIdAndUpdate(request.buyer, { $pull: { cart: request.product } });
-        if (!user) {
-            console.error('User not found');
-            return;
-        }
-
-        // notify
-        const dealCancellationMessageByOwner = `The deal for the product has been cancelled by the owner`;
-        const dealCancellationNotificationByOwner = new Notification({ user: request.buyer, message: dealCancellationMessageByOwner });
-        await dealCancellationNotificationByOwner.save();
-        await User.findByIdAndUpdate(request.buyer, { $push: { notif: dealCancellationNotificationByOwner._id } });
-        break;
-    }
+                    {
+                        const acceptedRequest = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'accepted' }, { new: true });
+                        if (!acceptedRequest) {
+                            console.error('Request not found');
+                            return;
+                        }
                 
-    case 'CANCEL_DEAL_USER':
-    {
-        const request = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'cancelled' }, { new: true });
-        if (!request) {
-            console.error('Request not found');
-            return;
-        }
+                        // product status
+                        await Product.findByIdAndUpdate(acceptedRequest.product, { status: true });
+                
+                        // add to cart
+                        const user = await User.findById(acceptedRequest.buyer);
+                        user.cart.push(acceptedRequest.product);
+                        await user.save();
+                
+                        //remove req
+                        await User.findByIdAndUpdate(acceptedRequest.buyer, { $pull: { outgoingRequests: acceptedRequest._id } });
+                        await User.findByIdAndUpdate(acceptedRequest.owner, { $pull: { incomingRequests: acceptedRequest._id } });
+                
+                        // auto reject req
+                        const otherRequests = await Request.find({ product: acceptedRequest.product, status: 'pending' });
+                        for (const req of otherRequests) {
+                            await Request.findByIdAndUpdate(req._id, { status: 'rejected' });
+                            await User.findByIdAndUpdate(req.buyer, { $pull: { outgoingRequests: req._id } });
+                            await User.findByIdAndUpdate(req.owner, { $pull: { incomingRequests: req._id } });
+                            const rejectionMessage = `Your request for the product has been rejected`;
+                            const rejectionNotification = new Notification({ user: req.buyer, message: rejectionMessage });
+                            await rejectionNotification.save();
+                            await User.findByIdAndUpdate(req.buyer, { $push: { notif: rejectionNotification._id } });
+                        }
 
-        // upd prod status
-        const product = await Product.findByIdAndUpdate(request.product, { status: false });
-        if (!product) {
-            console.error('Product not found');
-            return;
-        }
+                        // clear prod req array
+                        await Product.findByIdAndUpdate(acceptedRequest.product, { requests: [] });
 
-        // remove
-        const user = await User.findByIdAndUpdate(request.buyer, { $pull: { cart: request.product } });
-        if (!user) {
-            console.error('User not found');
-            return;
-        }
+                        // notify
+                        const notificationMessage = `Your request for the product has been accepted`;
+                        const notification = new Notification({ user: acceptedRequest.buyer, message: notificationMessage });
+                        await notification.save();
+                        await User.findByIdAndUpdate(acceptedRequest.buyer, { $push: { notif: notification._id } });
 
-        // notify
-        const dealCancellationMessageByUser = `The deal for your product has been cancelled by the user`;
-        const dealCancellationNotificationByUser = new Notification({ user: product.user, message: dealCancellationMessageByUser });
-        await dealCancellationNotificationByUser.save();
-        await User.findByIdAndUpdate(product.user, { $push: { notif: dealCancellationNotificationByUser._id } });
-        break;
-    }
+                        break;
+                    }
+                
+                    case 'REJECT_REQUEST':
+                    {
+                        const rejectedRequest = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'rejected' }, { new: true });
+                        if (!rejectedRequest) {
+                            console.error('Request not found');
+                            return;
+                        }
+                
+                        // remove req
+                        await User.findByIdAndUpdate(rejectedRequest.buyer, { $pull: { outgoingRequests: rejectedRequest._id } });
+                        await Product.findByIdAndUpdate(rejectedRequest.product, { $pull: { requests: rejectedRequest._id } });
+                        await User.findByIdAndUpdate(rejectedRequest.owner, { $pull: { incomingRequests: rejectedRequest._id } });
+                
+                        // notify
+                        const rejectionMessage = `Your request for the product has been rejected`;
+                        const rejectionNotification = new Notification({ user: rejectedRequest.buyer, message: rejectionMessage });
+                        await rejectionNotification.save();
+                        await User.findByIdAndUpdate(rejectedRequest.buyer, { $push: { notif: rejectionNotification._id } });
+                        break;
+                    }
+                
+                    case 'REQUEST_REQUEST':
+                    {
+                        const product = await Product.findById(parsedMessage.productId);
+                        if (!product) {
+                            console.error('Product not found');
+                            return;
+                        }
+                
+                        if (product.status) {
+                            console.error('Request cannot be sent, product is sold');
+                            return;
+                        }
+                
+                        const request = new Request({
+                            owner: product.user,
+                            buyer: parsedMessage.from,
+                            product: parsedMessage.productId
+                        });
+                        await request.save();
+                
+                        // add request to arrays
+                        await User.findByIdAndUpdate(parsedMessage.from, { $push: { outgoingRequests: request._id } });
+                        await Product.findByIdAndUpdate(parsedMessage.productId, { $push: { requests: request._id } });
+                        await User.findByIdAndUpdate(product.user, { $push: { incomingRequests: request._id } });
+                
+                        // notify
+                        const requestNotificationMessage = `You have received a request for your product`;
+                        const requestNotification = new Notification({ user: product.user, message: requestNotificationMessage });
+                        await requestNotification.save();
+                        await User.findByIdAndUpdate(product.user, { $push: { notif: requestNotification._id } });
+                        break;
+                    }
+                
+                    case 'CANCEL_DEAL_OWNER':
+                    {
+                        const request = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'cancelled' }, { new: true });
+                        if (!request) {
+                            console.error('Request not found');
+                            return;
+                        }
+                
+                        // upd prod status
+                        const product = await Product.findByIdAndUpdate(request.product, { status: false });
+                        if (!product) {
+                            console.error('Product not found');
+                            return;
+                        }
+                
+                        // remove from cart
+                        const user = await User.findByIdAndUpdate(request.buyer, { $pull: { cart: request.product } });
+                        if (!user) {
+                            console.error('User not found');
+                            return;
+                        }
+                
+                        // notify
+                        const dealCancellationMessageByOwner = `The deal for the product has been cancelled by the owner`;
+                        const dealCancellationNotificationByOwner = new Notification({ user: request.buyer, message: dealCancellationMessageByOwner });
+                        await dealCancellationNotificationByOwner.save();
+                        await User.findByIdAndUpdate(request.buyer, { $push: { notif: dealCancellationNotificationByOwner._id } });
+                        break;
+                    }
+                
+                    case 'CANCEL_DEAL_USER':
+                    {
+                        const request = await Request.findByIdAndUpdate(parsedMessage.requestId, { status: 'cancelled' }, { new: true });
+                        if (!request) {
+                            console.error('Request not found');
+                            return;
+                        }
+                
+                        // upd prod status
+                        const product = await Product.findByIdAndUpdate(request.product, { status: false });
+                        if (!product) {
+                            console.error('Product not found');
+                            return;
+                        }
+                
+                        // remove from cart
+                        const user = await User.findByIdAndUpdate(request.buyer, { $pull: { cart: request.product } });
+                        if (!user) {
+                            console.error('User not found');
+                            return;
+                        }
+                
+                        // notify
+                        const dealCancellationMessageByUser = `The deal for your product has been cancelled by the user`;
+                        const dealCancellationNotificationByUser = new Notification({ user: product.user, message: dealCancellationMessageByUser });
+                        await dealCancellationNotificationByUser.save();
+                        await User.findByIdAndUpdate(product.user, { $push: { notif: dealCancellationNotificationByUser._id } });
+                        break;
+                    }
         default:
             console.log('Unknown message type:', parsedMessage.type);
         }
